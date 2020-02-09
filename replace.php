6b42a0a97e833f906d22109b69fb87ce3d1af77e
Matt Glosson <matt.glosson@gmail.com>
2020-02-09 10:24:21 -0600

    Putting on GitHub

--- replace.php
+++ replace.php
@@ -1,11 +1,14 @@
 <?php
-$orig = "पीछले बार";
-$repl = "पिछली बार";
+
+# JUST DO THIS IN NOTEPAD++
+
+$orig = "\\v 9 اِس حصے کا کِیا کِیا ہم سبق ہے؟";
+$repl = "\v 9 اِس حصے کا کِیا کِیا اہم سبق ہے؟";
 
 if (!isset($argv[1])) {
- echo "Syntax: replace.php <filename> <source-text> <replacement-text>\n";
+ echo "Syntax: replace.php <filename>\n";
  exit;
 } else {
- $filename = argv[1];
- file_put_contents($argv[1],preg_replace("/{$orig/",$repl,file_get_contents($argv[1])));
+ $filename = $argv[1];
+ file_put_contents($filename,preg_replace("/$orig/",$repl,file_get_contents($filename)));
 }
